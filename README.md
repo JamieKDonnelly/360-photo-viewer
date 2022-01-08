911563289ff18b6a3d34b9604fb20f66fac71e0c
JKD <jdonnelly@riftgroup.com>
2022-01-08 10:54:53 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# 360-photo-viewer
+# 360-drone
+
+DEV
+`npx parcel src/index.html`
+
+Production
+`parcel build src/index.html`
+
+https://jamiekdonnelly.github.io/360-photo-viewer/
---
+++ src/script.js
@@ -0,0 +1,54 @@
+const myStorage = window.sessionStorage;
+
+function loadViewer(imageUrl) {
+  const viewer = document.getElementById("viewer");
+  // const PSV = new PhotoSphereViewer({
+  //   panorama: imageUrl,
+  //   container: viewer,
+  //   time_anim: 3000,
+  //   navbar: true,
+  //   usexmpdata: true,
+  //   navbar_style: {
+  //     backgroundColor: "rgba(58, 67, 77, 0.7)",
+  //   },
+  // });
+
+  var psv = new PhotoSphereViewer.Viewer({
+    container: viewer,
+    panorama: imageUrl,
+  });
+
+  viewer.classList.add("show");
+}
+
+function onUpload(event) {
+  const file = event.target.files[0];
+  // if (!file.type) {
+  //   status.textContent =
+  //     "Error: The File.type property does not appear to be supported on this browser.";
+  //   return;
+  // }
+  // if (!file.type.match("image.*")) {
+  //   status.textContent =
+  //     "Error: The selected file does not appear to be an image.";
+  //   return;
+  // }
+  const reader = new FileReader();
+  reader.addEventListener("load", (event) => {
+    loadViewer(event.target.result);
+  });
+  reader.readAsDataURL(file);
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  const imageInput = document.getElementById("imageInput");
+  const buttons = document.querySelectorAll(".load-image-button");
+
+  imageInput.addEventListener("change", onUpload);
+  buttons.forEach((button) => {
+    const imageUrl = button.getAttribute("data-image-url");
+    button.addEventListener("click", () => {
+      loadViewer(imageUrl);
+    });
+  });
+});
